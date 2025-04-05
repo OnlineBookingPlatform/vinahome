deaeac7dc5d5da46a2d59020594d07ee2080f4be
minwang03 <93901911+minwang03@users.noreply.github.com>
2025-04-05 22:41:48 +0700

    Update payment-method.vue

--- pages/(payment)/payment-method.vue
+++ pages/(payment)/payment-method.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 const showPaymentMethods = ref(false)
+const showTripInfo = ref(false)
+const showFormTripInfo = ref(false)
 
 const paymentMethod = ref("qr")
 
@@ -30,7 +32,31 @@ const contactInfoForm = ref({
 
 const submitForm = () => {
   showPaymentMethods.value = true
+  showFormTripInfo.value = true
+  showTripInfo.value = true
 }
+
+const showPriceDetail = ref(true)
+
+
+const minutes = ref(15)
+const seconds = ref(0)
+const countdown = () => {
+  if (seconds.value === 0) {
+    if (minutes.value === 0) return
+    minutes.value--
+    seconds.value = 59
+  } else {
+    seconds.value--
+  }
+}
+const togglePriceDetail = () => {
+  showPriceDetail.value = !showPriceDetail.value
+}
+
+onMounted(() => {
+  setInterval(countdown, 1000)
+})
 </script>
 
 <template>
@@ -93,7 +119,115 @@ const submitForm = () => {
       <button class="button-gradient h-[50px] w-full text-white text-lg font-bold rounded-lg hover:brightness-75"
         @click="submitForm">Thanh toán</button>
     </div>
-    <aside class="min-w-56">
+
+    <aside class="min-w-72 ml-10 space-y-4">
+      <!--Form thời gian giữ vé -->
+      <div class="bg-white border rounded-xl p-4 text-center shadow">
+        <p class="text-base font-semibold text-black mb-3">Thời gian giữ vé còn lại</p>
+        <div class="flex justify-center items-center gap-4 font-bold text-white text-xl">
+          <div class="flex flex-col items-center">
+            <div class="bg-[#38c7f3] rounded-lg px-3 py-1 min-w-[44px] text-center">
+              {{ minutes.toString().padStart(2, '0') }}
+            </div>
+            <span class="text-xs text-muted mt-1">phút</span>
+          </div>
+          <div class=" justify-center text-[#38c7f3] text-xl font-bold">
+            <span class="leading-none h-10 flex ">:</span>
+          </div>
+          <div class="flex flex-col items-center">
+            <div class="bg-[#38c7f3] rounded-lg px-3 py-1 min-w-[44px] text-center">
+              {{ seconds.toString().padStart(2, '0') }}
+            </div>
+            <span class="text-xs text-muted mt-1">giây</span>
+          </div>
+        </div>
+      </div>
+
+      <!--Form số tiền cần thanh toán -->
+      <div class="bg-white border rounded-xl shadow">
+        <div class="p-4 border-b flex justify-between items-center cursor-pointer" @click="togglePriceDetail">
+          <p class="font-semibold text-xl">Số tiền cần thanh toán</p>
+          <div class="flex items-center justify-center bg-[#38c7f3] p-2 rounded-full">
+            <Icon :name="showPriceDetail ? 'mdi:chevron-up' : 'mdi:chevron-down'" class="text-white" />
+          </div>
+        </div>
+        <div v-show="showPriceDetail" class="p-4 text-sm space-y-2 text-muted">
+          <div class="flex justify-between">
+            <span class="text-gray-600">Giá vé</span>
+            <span class="font-bold text-black">130.000đ x 2</span>
+          </div>
+          <div class="flex justify-between text-xs text-gray-400 italic">
+            <span>Giá gốc</span>
+            <span>Mã ghế/giường: 3, 4</span>
+          </div>
+          <div class="flex justify-between">
+            <span class="text-gray-600">Khuyến mãi</span>
+            <span class="text-green-500">-20.000đ</span>
+          </div>
+        </div>
+        <div class="p-4 border-t font-bold text-white bg-[#38c7f3] flex justify-between rounded-b-xl">
+          <span>Tổng tạm tính</span>
+          <span>240.000đ</span>
+        </div>
+      </div>
+
+      <!--Form số tiền cần thanh toán -->
+      <div class="bg-white rounded-xl shadow border text-sm" v-if="showFormTripInfo">
+        <div class="p-4 border-b flex justify-between items-center cursor-pointer"
+          @click="showTripInfo = !showTripInfo">
+          <p class="font-semibold text-xl">Thông tin chuyến đi</p>
+          <div class="flex items-center justify-center bg-[#38c7f3] p-2 rounded-full">
+            <Icon :name="showTripInfo ? 'mdi:chevron-up' : 'mdi:chevron-down'" class="text-white" />
+          </div>
+        </div>
+
+        <div v-show="showTripInfo" class="p-4 space-y-4">
+          <div class="rounded-xl overflow-hidden">
+            <img class="w-full object-cover" src="https://placehold.co/10x10" alt="bus image">
+          </div>
+          <div>
+            <h5 class="text-base font-semibold">{{ mockTripDetail.destination }}</h5>
+            <p class="text-muted">{{ mockTripDetail.bus }}</p>
+          </div>
+
+          <div class="relative ">
+            <!-- Đường dọc -->
+            <div class="absolute top-[22px] left-[7px] h-[60px] w-px bg-gray-400"></div>
+
+            <!-- Điểm đón -->
+            <div class="flex gap-2 items-start mb-6">
+              <div class="size-4 border-2 border-black rounded-full mt-1 z-10 bg-white"></div>
+              <div>
+                <div class="flex gap-2 items-center font-semibold">
+                  <span>{{ mockTripDetail.pickupInfo.time }}</span>
+                  <span>{{ mockTripDetail.pickupInfo.locationName }}</span>
+                </div>
+                <p class="text-muted text-xs">{{ mockTripDetail.pickupInfo.address }}</p>
+                <button class="text-primary text-sm underline">Thay đổi</button>
+              </div>
+            </div>
+
+            <!-- Điểm trả -->
+            <div class="flex gap-2 items-start">
+              <div class="size-4 mt-1 text-black z-10 flex items-center justify-center">
+                <Icon name="fa6-solid:location-dot" class="size-4" />
+              </div>
+              <div>
+                <div class="flex gap-2 items-center font-semibold">
+                  <span>{{ mockTripDetail.returnInfo.time }}</span>
+                  <span>{{ mockTripDetail.returnInfo.locationName }}</span>
+                </div>
+                <p class="text-muted text-xs">{{ mockTripDetail.returnInfo.address }}</p>
+                <button class="text-primary text-sm underline">Thay đổi</button>
+              </div>
+            </div>
+          </div>
+
+          <div class="text-sm font-medium">
+            Ghế: <span class="font-semibold">{{ mockTripDetail.seats }} khách</span>
+          </div>
+        </div>
+      </div>
 
     </aside>
   </section>
