1fac57e978b2c74cb17321f6b15bc59fc9cf5ab8
Đặng Tuấn Thành <tuanthanhktltk21@gmail.com>
2025-04-28 19:04:28 +0700

    init form search ticket

--- pages/tra-cuu-ve/index.vue
+++ pages/tra-cuu-ve/index.vue
@@ -1,46 +1,97 @@
 <script setup lang="ts">
-import {lookUpTicketsList} from "~/mocks/tra-cuu";
+import type { FormInstance, FormRules } from "element-plus";
+import { lookUpTicketsList } from "~/mocks/tra-cuu";
 
-const phoneRegex = new RegExp(
-  /^(?:\+?\d{1,3}[-.\s]?)?(?:\(?\d{1,4}\)?[-.\s]?)?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/
-);
+// const phoneRegex = new RegExp(
+//   /^(?:\+?\d{1,3}[-.\s]?)?(?:\(?\d{1,4}\)?[-.\s]?)?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/
+// );
 
-const lookUpModel = reactive({
-  orderCode: "",
-  phoneNumber: "",
-});
+// const lookUpModel = reactive({
+//   orderCode: "",
+//   phoneNumber: "",
+// });
 
-const resultTicket = ref("");
+// const resultTicket = ref("");
 
-const onSubmit = () => {
-  const { phoneNumber, orderCode } = lookUpModel;
-  if (!!phoneNumber && !phoneRegex.test(phoneNumber)) {
-    alert("Sai format số điện thoại");
-    return;
-  }
+// const onSubmit = () => {
+//   const { phoneNumber, orderCode } = lookUpModel;
+//   if (!!phoneNumber && !phoneRegex.test(phoneNumber)) {
+//     alert("Sai format số điện thoại");
+//     return;
+//   }
 
-  let ticket;
+//   let ticket;
 
-  if (
-    (ticket = lookUpTicketsList.find(
-      (ticket) =>
-        ticket.orderCode === orderCode || ticket.passenger.phone === phoneNumber
-    ))
-  ) {
-    resultTicket.value = JSON.stringify(ticket);
-  } else {
-    alert("Không tìm được vé");
-  }
-};
+//   if (
+//     (ticket = lookUpTicketsList.find(
+//       (ticket) =>
+//         ticket.orderCode === orderCode || ticket.passenger.phone === phoneNumber
+//     ))
+//   ) {
+//     resultTicket.value = JSON.stringify(ticket);
+//   } else {
+//     alert("Không tìm được vé");
+//   }
+// };
+interface RuleForm {
+  phone: string;
+  code: string;
+}
+const ruleFormRef = ref<FormInstance>()
+const ruleForm = reactive<RuleForm>({
+  phone: '',
+  code: '',
+})
+const rules = reactive<FormRules<RuleForm>>({
+  phone: [
+    { required: true, message: 'Vui lòng nhập số điện thoại', trigger: 'blur' },
+  ],
+  code: [
+    { required: true, message: 'Vui lòng nhập mã vé', trigger: 'blur' },
+  ],
+})
+const submitForm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+}
 </script>
 
 <template>
-  <div class="container my-[60px] space-y-6 text-foreground">
-    <p>
+  <div class="container space-y-6 justify-center text-center mt-10 mb-20">
+    <h1 class="text-xl font-bold">TRA CỨU THÔNG TIN ĐẶT VÉ</h1>
+
+    <el-form ref="ruleFormRef" style="max-width: 500px" class="mx-auto" :model="ruleForm" :rules="rules">
+      <el-form-item prop="phone">
+        <span>Số điện thoại</span>
+        <el-input v-model="ruleForm.phone" type="text" autocomplete="off" />
+      </el-form-item>
+      <el-form-item prop="code">
+        <span>Mã vé</span>
+        <el-input v-model="ruleForm.code" type="text" autocomplete="off" />
+      </el-form-item>
+      <el-form-item class="mt-10">
+        <div class="w-full flex justify-center items-center gap-4">
+          <el-button type="primary" round @click="submitForm(ruleFormRef)" class="w-[200px]">
+          Tra cứu
+        </el-button>
+        </div>
+      </el-form-item>
+
+    </el-form>
+  </div>
+
+
+  <!-- <p>
       <NuxtLink to="/">Trang chủ</NuxtLink> >
       <span class="underline decoration-primary">Tra cứu vé xe</span>
-    </p>
-    <form
+    </p> -->
+  <!-- <form
       class="mx-auto border border-black/20 rounded-2xl p-3 mt-4 w-fit"
       @submit.prevent="onSubmit"
     >
@@ -75,9 +126,13 @@ const onSubmit = () => {
       >
         Tra cứu
       </button>
-    </form>
-    {{ resultTicket }}
-  </div>
+    </form> -->
+  <!-- {{ resultTicket }} -->
+
 </template>
 
-<style scoped></style>
+<style>
+/* .custom-button>.el-form-item__content {
+  justify-content: center !important;
+} */
+</style>
