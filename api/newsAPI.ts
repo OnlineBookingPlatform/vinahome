a8dfc2bda6b9aa2f730707fa686ce428dfd01ed8
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2025-05-01 05:02:48 +0700

    feat: get ListReceive News API

--- api/newsAPI.ts
+++ api/newsAPI.ts
@@ -1,21 +1,36 @@
-import type { DTO_RQ_NewsType } from "~/types/NewsType";
+import type { DTO_RQ_NewsType, NewsType } from "~/types/NewsType";
 import type { ApiResponse } from "./APIResponse";
 
 export const registerReceiveNewsAPI = async (
     data: DTO_RQ_NewsType
-  ): Promise<ApiResponse<void>> => {
+): Promise<ApiResponse<void>> => {
     const config = useRuntimeConfig();
     const API_GATEWAY_URL = config.public.apiGatewayUrl;
     try {
-      return await $fetch<ApiResponse<void>>(
-        `${API_GATEWAY_URL}/v1/news/register-receive-news`,
-        {
-          method: "POST",
-          body: data,
-        }
-      );
+        return await $fetch<ApiResponse<void>>(
+            `${API_GATEWAY_URL}/v1/news/register-receive-news`,
+            {
+                method: "POST",
+                body: data,
+            }
+        );
     } catch (error) {
-      console.error("Register Receive News failed: ", error);
-      throw error;
+        console.error("Register Receive News failed: ", error);
+        throw error;
     }
-  }
+}
+export const getListReceiveNewsAPI = async (): Promise<ApiResponse<NewsType[]>> => {
+    const config = useRuntimeConfig();
+    const API_GATEWAY_URL = config.public.apiGatewayUrl;
+    try {
+        return await $fetch<ApiResponse<NewsType[]>>(
+            `${API_GATEWAY_URL}/v1/news/get-list-receive-news`,
+            {
+                method: "GET",
+            }
+        );
+    } catch (error) {
+        console.error("Get List Receive News failed: ", error);
+        throw error;
+    }
+}
--- pages/super-admin/receive-news.vue
+++ pages/super-admin/receive-news.vue
@@ -1,18 +1,44 @@
 <script setup lang="ts">
+import { ElMessage } from 'element-plus';
+import { getListReceiveNewsAPI } from '~/api/newsAPI';
 import type { NewsType } from '~/types/NewsType';
 
 definePageMeta({
     layout: "superadmin",
 });
 const tableData = ref<NewsType[]>([]);
+const isLoading = ref(false);
+const fetchReceiveNews = async () => {
+    isLoading.value = true;
+    try {
+        const response = await getListReceiveNewsAPI();
+        if (response.status === 200) {
+            tableData.value = response.result;
+            console.log('Received news data:', tableData.value);
+        } else {
+            ElMessage.error(response.message || 'Lỗi khi tải dữ liệu');
+        }
+    } catch (error) {
+        ElMessage.error('Lỗi hệ thống, vui lòng thử lại sau.');
+    } finally {
+        isLoading.value = false;
+    }
+};
+onMounted(() => {
+    fetchReceiveNews();
+});
 
 </script>
 <template>
     <section class="p-2">
-        <el-table :data="tableData" stripe style="width: 100%">
+        <el-table :data="tableData" stripe style="width: 100%" v-loading="isLoading">
             <el-table-column type="index" label="STT" width="50" />
             <el-table-column prop="email" label="Email" />
-            <el-table-column prop="created_at" label="Ngày đăng ký" />
+            <el-table-column prop="createdAt" label="Ngày đăng ký">
+                <template #default="scope">
+                    {{ new Date(scope.row.createdAt).toLocaleDateString() }}
+                </template>
+            </el-table-column>
         </el-table>
     </section>
 </template>
